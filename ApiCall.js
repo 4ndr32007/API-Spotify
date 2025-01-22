b077848206d5a9b6814b31d2df880387eac29671
RaffMonge <monge.cuniglia.raffaele.1gr@denina.it>
2025-01-22 11:41:44 +0100

    Update ApiCall.js

--- ApiCall.js
+++ ApiCall.js
@@ -1,19 +1,24 @@
-//questa pagina viene mostrata dopo che l'utente ha fatto l'accesso con il suo account
+/*
+questa pagina viene mostrata dopo che l'utente ha fatto l'accesso con il suo account
+*/
 import React, { useEffect } from 'react'
 
 const BaseUrl="https://api.spotify.com/v1/me"
 
 const ApiCall = () => {
 	useEffect(() => {
-		//appena viene redirezionato verso questa pagina avverrà la chiamata alle API, il token va recuperato dall'URL
-		//mi permette di prendere la stringa che compone il token a partire dal "#" che segnala l'inizio del token
+		/*
+Appena viene redirezionato verso questa pagina avverrà la chiamata alle API, il token va recuperato dall'URL e mi permette di prendere la stringa che compone il token a partire dal "#" che segnala l'inizio del token
+  */
 		const rawToken = window.location.hash;
 		console.log(rawToken)
 		
 		if (rawToken) {
-			// Rimuovo il simbolo '#' e ottiengo il token, ma ci sono ancora altri parametri che non servono
-			//la funzione URLSearchParams() permette di analizzare automaticamente l'URL che gli viene 
-			//passato e in automantico ne restituisce i rispettivi chave valore, in maniera tale da facilitare la ricerca del token
+			//Rimuovo il simbolo '#' e ottiengo il token, ma ci sono ancora altri parametri che non servono
+			
+/*
+La funzione URLSearchParams() permette di analizzare automaticamente l'URL che gli viene  passato e in automantico ne restituisce i rispettivi chave valore, in maniera tale da facilitare la ricerca del token
+*/
 			const parametri = new URLSearchParams(rawToken.substring(1));
 			//cerca nel URL quindi in parametri una chiave di valore "access_token" e ne restituisci il valore nella variabile accessToken
 			const accessToken = parametri.get("access_token");
