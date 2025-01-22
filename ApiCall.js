21afeb7699879dc860005dc832ecb3442abdd78b
RaffMonge <monge.cuniglia.raffaele.1gr@denina.it>
2025-01-22 11:42:40 +0100

    Update ApiCall.js

--- ApiCall.js
+++ ApiCall.js
@@ -7,23 +7,23 @@ const BaseUrl="https://api.spotify.com/v1/me"
 
 const ApiCall = () => {
 	useEffect(() => {
-		/*
+/*
 Appena viene redirezionato verso questa pagina avverrà la chiamata alle API, il token va recuperato dall'URL e mi permette di prendere la stringa che compone il token a partire dal "#" che segnala l'inizio del token
-  */
+*/
 		const rawToken = window.location.hash;
 		console.log(rawToken)
 		
 		if (rawToken) {
-			//Rimuovo il simbolo '#' e ottiengo il token, ma ci sono ancora altri parametri che non servono
+//Rimuovo il simbolo '#' e ottiengo il token, ma ci sono ancora altri parametri che non servono
 			
 /*
 La funzione URLSearchParams() permette di analizzare automaticamente l'URL che gli viene  passato e in automantico ne restituisce i rispettivi chave valore, in maniera tale da facilitare la ricerca del token
 */
 			const parametri = new URLSearchParams(rawToken.substring(1));
-			//cerca nel URL quindi in parametri una chiave di valore "access_token" e ne restituisci il valore nella variabile accessToken
+//cerca nel URL quindi in parametri una chiave di valore "access_token" e ne restituisci il valore nella variabile accessToken
 			const accessToken = parametri.get("access_token");
 			console.log("Access Token:", accessToken);
-			//una volta ottenuto il token si può fare la chiamata alle API		
+//una volta ottenuto il token si può fare la chiamata alle API		
 		}
 
 		//API call
