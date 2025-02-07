749a42814663668b9caee8a7f96bd7d89b67fa3b
4ndr32007 <giraudo.andrea.1gr@denina.it>
2025-02-07 17:31:33 +0100

    Add files via upload

---
+++ spotify/src/App.js
@@ -0,0 +1,38 @@
+import React, { createContext, useState } from 'react'
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import HomePage from "./HomePage"
+import Login from "./Login"
+import Errore from "./Errore"
+import ApiCall from "./ApiCall"
+import TopArtisti from './TopArtisti'
+import Home2 from './HomePage2'
+import Suggerimento from './Suggerimento'
+
+export const UserContext = createContext()
+
+const App = () => {
+	//questi dati devono essere visibili a tutti i file
+	const [data, setData] = useState(null)
+	const [token, setToken] = useState(null)
+	
+	return (
+		<div>
+		{/* avvolgo TUTTE le route con il provider perchè devono avere l'accesso alla risorsa condivisa */}
+			<UserContext.Provider value={{ data, setData, token, setToken }}>
+				<BrowserRouter>
+					<Routes>
+						<Route path="/" element={<HomePage />} />
+						<Route path="/login" element={<Login />} />
+						<Route path="/callback" element={<ApiCall />} />
+						<Route path="/top-artisti" element={<TopArtisti />} />
+						<Route path="/funzioni" element={<Home2 />} />
+						<Route path="/canzoni-consigliate" element={<Suggerimento />} />
+						<Route path="/*" element={<Errore />} />
+					</Routes>
+				</BrowserRouter>
+			</UserContext.Provider>
+		</div>
+	)
+}
+
+export default App
