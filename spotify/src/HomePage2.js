f586e3ea5f361deae74665c5e8cde7a1a232c820
4ndr32007 <giraudo.andrea.1gr@denina.it>
2025-02-07 17:27:18 +0100

    Add files via upload

---
+++ spotify/src/HomePage2.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import './HomePage2.css'
+
+const SecondHomePage = () => {
+	const apri = useNavigate()
+
+	const Naviga = (pag) => {
+		apri(`/${pag}`)
+	}
+
+	return (
+		<div id="contanier">
+			
+			<div id="testo">
+				<h1 id="titolo">Benvenuto nella Dashboard</h1>
+				<p>Seleziona una delle opzioni per esplorare le funzionalità del sito</p>
+				<input type="button" value="Visualizza Artisti Preferiti" className="btn" onClick={() => Naviga('top-artisti')} />
+				<input type="button" value="Brani consigliati" className="btn" onClick={() => Naviga('canzoni-consigliate')} />
+			</div>
+		</div>
+	)
+}
+
+export default SecondHomePage
+
+
+/*
+	->🌍 Ottieni le classifiche globali
+			-Recupera le playlist più ascoltate di un paese
+				Endpoint: /v1/browse/categories/{category_id}/playlists
+	->📊 Scoprire contenuti popolari
+			-Recupera le playlist editoriali di Spotify
+				Endpoint: /v1/browse/featured-playlists
+			
+			-Recupera i nuovi album pubblicati
+				Endpoint: /v1/browse/new-releases
+			
+			-Recupera le categorie di Spotify (es: "rock", "hip-hop", "chill")
+				Endpoint: /v1/browse/categories
+			
+*/
